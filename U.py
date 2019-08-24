033ba037afa24fef5f7e42ee1148e315d4e4023a
bkashefipour <43657153+bkashefipour@users.noreply.github.com>
2019-08-23 17:22:23 -0700

    test

--- U.py
+++ U.py
@@ -3,6 +3,37 @@ class ListNode(object):
   def __init__(self, x):
     self.val = x
     self.next = None
+class Node: 
+  # Constructor to initialize the node object 
+  def __init__(self, data): 
+    self.data = data 
+    self.next = None
+class LinkedList: 
+  # Function to initialize head 
+  def __init__(self): 
+    self.head = None
+  # Function to reverse the linked list 
+  def reverse(self): 
+    prev = None
+    current = self.head 
+    while(current is not None): 
+      next = current.next
+      current.next = prev 
+      prev = current 
+      current = next
+    self.head = prev 
+  # Function to insert a new node at the beginning 
+  def push(self, new_data): 
+    new_node = Node(new_data) 
+    new_node.next = self.head 
+    self.head = new_node 
+  
+  # Utility function to print the linked LinkedList 
+  def printList(self): 
+    temp = self.head 
+    while(temp): 
+      print(temp.data) 
+      temp = temp.next
 
 class Solution:
   variable = "blah"
@@ -25,7 +56,6 @@ class Solution:
 
 
     return True
-
   def mergeTwoLists(self, l1, l2):
     if l1 is None:
       return l2
@@ -37,7 +67,6 @@ class Solution:
     else:
       l2.next = self.mergeTwoLists(l1, l2.next)
       return l2
-
   def mergeTwoListsIterative(self, l1, l2):
     acurrent = None
     aroot = None
@@ -65,15 +94,12 @@ class Solution:
         acurrent.next = aanextNode
       acurrent = aanextNode
     return aroot
-
   def function(self):
         print("This is a message inside the class.")
-  
   def printList(self, dd):
     while dd:
       print(dd.val)
       dd= dd.next
-
   def print_backward(self,list):
     if list == None: return
     head = list
@@ -81,25 +107,48 @@ class Solution:
     self.print_backward(tail)
     print(head.val)
     
+  def revFU(self,list):
+    current = list
+    prev = None
+    while (current != None):
+      next_Node = current.next
+      current.next = prev
+      prev = current
+      current = next_Node
+    
+    return prev
+    
   def rev(self,list):
-    if list == None : 
-      return
-    else :
-      newNode = ListNode(list.val)
-      if list.next == None:
-        return newNode
-      else :
-        
-        retunr 
+    current = list
+    builder = None
+    nodeA = None
+    builder = ListNode(current.val)
+    rev = builder
+    current = current.next
+    builder = ListNode(current.val)
+    builder.next = rev
+    rev = builder
+    current = current.next
+    builder = ListNode(current.val)
+    builder.next = rev
+    rev = builder
+    return rev
+
     
-    if list.next == None : return newNode
-    rest = list.next
-    tsil = self.rev(list.next)
-    rest.next = newNode
-    print('s')
-    return tsil
+
+
+
+
+  def copyFirst(self,list):
+    if 
+
     
-      
+    # end_of_rest = list.next
+    # if end_of_rest == None:
+    #   return list
+    # else:
+    #   end_of_rest.next = newNode
+    
 
 
   def reverse(self, l):
@@ -116,22 +165,26 @@ class Solution:
       list_rest_begin.next = list_first
 
     return list_rest_begin
-
-  def Make_first_of_l2__first_of_l1(self, l1, l2):
-    if l2 == None:
-      return
-    
-    l3 = l2.next
-    l2.next = l1
-    l1 = l2
-    l2 = l3
-    print('sdsd')
-      
+  
   def push(self, l ,new_data): 
     ph = l
     l = ListNode(new_data) 
     new_node.next =  l
     l = new_node
+
+
+# llist = LinkedList() 
+# llist.push(20) 
+# llist.push(4) 
+# llist.push(15) 
+# llist.push(85) 
+  
+# print("Given Linked List")
+# llist.printList() 
+# llist.reverse() 
+# print("\nReversed Linked List")
+# llist.printList() 
+  
 
 # myobjectx = Solution()
 # myobjectx.variable
@@ -143,16 +196,15 @@ a.next.next = ListNode(16)
 # b.next = ListNode(4)
 # b.next.next = ListNode(6)
 # c = Solution().mergeTwoLists(a,b)
-d = ListNode(111)
-d.next = ListNode(222)
-#d.next.next = ListNode(333)
+d = ListNode('AA')
+d.next = ListNode('BB')
+d.next.next = ListNode('CC')
 Solution().printList(d)
 Solution().print_backward(d)
 print("---------------------")
 e = Solution().rev(d)
 Solution().printList(e)
 
-Solution().printList(d)
 
 #
 #e = Solution().reverse(d)
